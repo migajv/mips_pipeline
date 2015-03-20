cf23fc540429128d32c7812089f0302f2281eb0a
Junkang Ren <Junkang.Ren@silabs.com>
2015-03-20 10:27:22 -0500

    added missing files from last commit

---
+++ run.f
@@ -0,0 +1,11 @@
+-incdir verilog
+verilog/alu_control.v
+verilog/alu.v
+verilog/control.v
+verilog/cpu.v
+verilog/dm.v
+verilog/im.v
+verilog/pc.v
+verilog/regm.v
+verilog/regr.v
+verilog/test/cpu_tb.v
